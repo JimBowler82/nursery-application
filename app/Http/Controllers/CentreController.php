df34edd68517f802558a27d888549d338247fac5
jimbowler82 <j.bowler2017@gmail.com>
2022-08-06 13:47:48 +0100

    progress upto tma03

--- app/Http/Controllers/CentreController.php
+++ app/Http/Controllers/CentreController.php
@@ -8,8 +8,6 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
 class CentreController extends Controller
 {
